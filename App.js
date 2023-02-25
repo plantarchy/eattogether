391238eaf5c060d30834586fe14cdd3d46dfd773
bbworld1 <vwangsf@gmail.com>
2023-02-25 00:35:52 -0500

    Add navigator

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const TopStack = createStackNavigator();
 
 export default function App() {
   return (
