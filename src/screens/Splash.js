2fb7d492838ff1dde13be327859f14c180b7c346
bbworld1 <vwangsf@gmail.com>
2023-02-25 04:44:21 -0500

    Add splash

---
+++ src/screens/Splash.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Dimensions,
+  ImageBackground,
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  Alert,
+  TouchableWithoutFeedback,
+  Keyboard,
+  SafeAreaView
+} from 'react-native';
+
+const Login = props => {
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 48, marginBottom: 32 }}>QuickEats</Text>
+      <TouchableOpacity onPress={() => {console.log("login")}} style={{...styles.button, backgroundColor: "#2BD55B", marginBottom: 16}}>
+        <Text style={{fontSize: 18, color: "#EEE"}}>Signup</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => {console.log("login")}} style={styles.button}>
+        <Text style={{fontSize: 18, color: "#EEE"}}>Login</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const DEVICE_WIDTH = Dimensions.get("window").width;
+const DEVICE_HEIGHT = Dimensions.get("window").height;
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    height: DEVICE_HEIGHT,
+    width: DEVICE_WIDTH,
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    paddingBottom: 0.2 * DEVICE_HEIGHT
+  },
+  button: {
+    padding: 16,
+    backgroundColor: "#666",
+    width: DEVICE_WIDTH * 0.7,
+    alignItems: "center",
+    textAlign: "center",
+    borderRadius: 15,
+    color: "#EEE"
+  },
+  textInput: {
+    width: DEVICE_WIDTH * 0.7,
+    padding: 16,
+    backgroundColor: "#666",
+    borderRadius: 15,
+  }
+});
+
+export default Login;
